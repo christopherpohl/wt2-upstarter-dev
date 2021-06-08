50a25e6131a0ef75f91e48260c088959ab9111f4
christopherpohl <31965409+christopherpohl@users.noreply.github.com>
2021-06-08 20:08:40 +0200

    Update Swipe.js

--- src/components/Swipe.js
+++ src/components/Swipe.js
@@ -1,82 +1,27 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
-
+// import TinderCard from '../react-tinder-card/index'
 import TinderCard from 'react-tinder-card'
-
-const Container = styled.View`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-`
-
-
-const Header = styled.Text`
-    color: #000;
-    font-size: 30px;
-    margin-bottom: 30px;
-`
-
-const CardContainer = styled.View`
-    width: 90%;
-    max-width: 260px;
-    height: 300px;
-`
-
-const Card = styled.View`
-    position: absolute;
-    background-color: #fff;
-    width: 100%;
-    max-width: 260px;
-    height: 300px;
-    shadow-color: black;
-    shadow-opacity: 0.2;
-    shadow-radius: 20px;
-    border-radius: 20px;
-    resize-mode: cover;
-`
-
-const CardImage = styled.ImageBackground`
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    border-radius: 20px;
-`
-
-const CardTitle = styled.Text`
-    position: absolute;
-    bottom: 0;
-    margin: 10px;
-    color: #fff;
-`
-
-const InfoText = styled.Text`
-    height: 28px;
-    justify-content: center;
-    display: flex;
-    z-index: -100;
-`
 
 const db = [
   {
     name: 'Richard Hendricks',
-    img: require('../img/CFO.jpg')
+    url: './img/back.jpg'
   },
   {
     name: 'Erlich Bachman',
-    img: require('../img/CFO.jpg')
+    url: './img/back.jpg'
   },
   {
     name: 'Monica Hall',
-    img: require('../img/CFO.jpg')
+    url: './img/back.jpg'
   },
   {
     name: 'Jared Dunn',
-    img: require('../img/CFO.jpg')
+    url: './img/back.jpg'
   },
   {
     name: 'Dinesh Chugtai',
-    img: require('../img/CFO.jpg')
+    url: './img/back.jpg'
   }
 ]
 
@@ -94,21 +39,21 @@ function Simple () {
   }
 
   return (
-    <Container>
-      <Header>React Native Tinder Card</Header>
-      <CardContainer>
+    <div>
+      <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
+      <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
+      <h1>React Tinder Card</h1>
+      <div className='cardContainer'>
         {characters.map((character) =>
-          <TinderCard key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
-            <Card>
-              <CardImage source={character.img}>
-                <CardTitle>{character.name}</CardTitle>
-              </CardImage>
-            </Card>
+          <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+            <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+              <h3>{character.name}</h3>
+            </div>
           </TinderCard>
         )}
-      </CardContainer>
-      {lastDirection ? <InfoText>You swiped {lastDirection}</InfoText> : <InfoText />}
-    </Container>
+      </div>
+      {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
+    </div>
   )
 }
 
