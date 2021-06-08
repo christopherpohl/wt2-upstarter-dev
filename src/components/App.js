3b18f93bd0c036cb0cb604a1e23296fc9793089c
christopherpohl <31965409+christopherpohl@users.noreply.github.com>
2021-06-08 20:00:52 +0200

    Swiper Implementation #1

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,6 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col} from 'react-bootstrap';
 
-import Titel from './Titel.js';
-import Menue from './Menue.js';
-import Willkommen from './Willkommen.js';
-import Leistungen from './Leistungen.js';
-import Mehr from './Mehr.js';
-import WusstenSieSchon from './WusstenSieSchon.js';
-import Kontakt from './Kontakt.js';
-import Impressum from './Impressum.js';
 
 
 function App() {
@@ -22,9 +14,7 @@ function App() {
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
         crossOrigin="anonymous"
       />
-      <Container fluid>
-        <Willkommen />
-      </Container>
+     
     </div>
   );
 }
