fb1638c57760699d2fa60843811189a02a6df927
NicoN1100 <nico.neumann@stud.hs-bochum.de>
2021-06-09 14:01:24 +0200

    -add footer img

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,28 +6,29 @@ function Footer() {
     <footer class="text-center text-white" classNAme="footer">
     <section class="lg-4">
      
-      <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
-        ><i class="fab fa-facebook-f"></i
-      ></a>
+     
+      
+      
+      <img width="10%" height="20%" src="https://www.pngkit.com/png/detail/10-100309_facebook-black-white-icon-facebook-face-book-png.png"></img
+      >
+    
+      <img width="10%" height="20%" src="https://www.pngkit.com/png/detail/10-100309_facebook-black-white-icon-facebook-face-book-png.png"></img
+      >
+      <img width="10%" height="20%" src="https://www.pngkit.com/png/detail/10-100309_facebook-black-white-icon-facebook-face-book-png.png"></img
+      >
+     
+
+      <img width="10%" height="20%" src="https://www.pngkit.com/png/detail/10-100309_facebook-black-white-icon-facebook-face-book-png.png"></img
+      >
+  
+      
 
       
-      <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
-        ><i class="fab fa-twitter"></i
-      ></a>
-
       
-      <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
-        ><i class="fab fa-google"></i
-      ></a>
+      
 
      
-      <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
-        ><i class="fab fa-instagram"></i
-      ></a>
-
-      <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
-        ><i class="fab fa-instagram"></i
-      ></a>
+      
 
     
     </section>
