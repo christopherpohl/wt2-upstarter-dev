0c330952993a4294d9fa34fa4bc8cd750f64fee7
NicoN1100 <nico.neumann@stud.hs-bochum.de>
2021-06-09 14:33:05 +0200

    -add login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,37 @@
+import React, { useRef, useState } from "react"
+import { Form, Button, Card, Alert } from "react-bootstrap"
 
-
-import React from 'react';
-
+import { Link, useHistory } from "react-router-dom"
 
 
 
 function Login() {
   return (
     
-    <form action="/swipe">
-      <label>
-        <p>Username</p>
-        <input type="text" />
-      </label>
-      <label>
-        <p>Password</p>
-        <input type="password" />
-      </label>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-14" >Log In</h2>
+          <Form action="/swipe">
+          
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email"/>
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Passwort</Form.Label>
+              <Form.Control type="password"/>
+            </Form.Group>
+            <input  className="loginbtn"type="submit" value="Login" />
+          </Form>
+          <div className="w-100 text-center mt-3">
+            <Link to="/forgot-password">Passwort vergessen?</Link>
+          </div>
+        </Card.Body>
+      </Card>
+     
+    
+    
+  
 
     
     
