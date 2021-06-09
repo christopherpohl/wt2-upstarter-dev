2999727050080a03d301d42b5ef2107efe3c7d4b
NicoN1100 <nico.neumann@stud.hs-bochum.de>
2021-06-09 13:28:35 +0200

    add Route: Login, Swipe

---
+++ src/components/Login.js
@@ -0,0 +1,34 @@
+
+
+import React from 'react';
+
+
+
+
+function Login() {
+  return (
+    
+    <form action="/swipe">
+      <label>
+        <p>Username</p>
+        <input type="text" />
+      </label>
+      <label>
+        <p>Password</p>
+        <input type="password" />
+      </label>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+
+    
+    
+
+    
+
+
+  ); 
+}
+
+export default Login;
