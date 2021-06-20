8bd187aa744ce31be0c87167038a866c8800be75
EmreIpcioglu <73101924+EmreIpcioglu@users.noreply.github.com>
2021-06-20 19:22:41 +0200

    tttt

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,59 +2,50 @@ import React from 'react';
 
 function Footer() {
   return (
-    
+
     <footer class="text-center text-white" classNAme="footer">
-    <section class="lg-4">
-     
-</section>
+      <section class="lg-4">
+
+      </section>
       <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
-        ><i class="fab fa-facebook-f">Instagram</i
-      ></a>
-
-     
-      
-      
-      <img width="10%" height="20%" src="https://www.pngkit.com/png/detail/10-100309_facebook-black-white-icon-facebook-face-book-png.png"></img
-      >
-    
-      <img width="10%" height="20%" src="https://www.pngkit.com/png/detail/10-100309_facebook-black-white-icon-facebook-face-book-png.png"></img
-      >
-      <img width="10%" height="20%" src="https://www.pngkit.com/png/detail/10-100309_facebook-black-white-icon-facebook-face-book-png.png"></img
-      >
-     
-
-
-      <img width="10%" height="20%" src="https://www.pngkit.com/png/detail/10-100309_facebook-black-white-icon-facebook-face-book-png.png"></img>
-      
-
-      <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
-        ><i class="fab fa-twitter">Facebook</i
-      ></a>
-
-      
-      <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
-        ><i class="fab fa-google">Twitter</i
-      ></a>
-
-     
-      <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
-        ><i class="fab fa-instagram">Youtube</i
-      ></a>
-
-      <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
-        ><i class="fab fa-instagram">LinkIN</i
+      ><i class="fab fa-facebook-f">Instagram</i
       ></a>
 
 
-      
-      
-      
 
-     
-      
 
-    
-    </section>
-    </}
+
+
+      <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
+      ><i class="fab fa-twitter">Facebook</i
+      ></a>
+
+
+      <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
+      ><i class="fab fa-google">Twitter</i
+      ></a>
+
+
+      <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
+      ><i class="fab fa-instagram">Youtube</i
+      ></a>
+
+      <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
+      ><i class="fab fa-instagram">LinkIN</i
+      ></a>
+
+
+
+
+
+
+
+
+
+
+
+    </footer >);
+}
+
 
 export default Footer;
