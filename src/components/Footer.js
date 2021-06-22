ae337f6fe99fca2937b6f6e11abe63ee5666ebd9
EmreIpcioglu <73101924+EmreIpcioglu@users.noreply.github.com>
2021-06-22 21:29:15 +0200

    SocialFollowFooter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
      
      
       
-      
+     {/*
       <img width="10%" height="20%" src="https://www.pngkit.com/png/detail/10-100309_facebook-black-white-icon-facebook-face-book-png.png"></img
       >
     
@@ -20,6 +20,7 @@ function Footer() {
 
       <img width="10%" height="20%" src="https://www.pngkit.com/png/detail/10-100309_facebook-black-white-icon-facebook-face-book-png.png"></img
       >
+     */}
   
       
 
--- src/components/SocialFollow.js
+++ src/components/SocialFollow.js
@@ -5,31 +5,32 @@ import {
     faYoutube,
     faFacebook,
     faTwitter,
-    faInstagram
+    faInstagram,
+    faGithub
   } from "@fortawesome/free-brands-svg-icons";
 
 export default function SocialFollow() {
   return (
     <div class="social-container">
       <h3>Social Follow</h3>
-      <a href="https://www.youtube.com/c/jamesqquick"
+      <a href="https://github.com/christopherpohl/wt2-upstarter-dev"
         className="youtube social">
-        <FontAwesomeIcon icon={faYoutube} size="2x" />
+        <FontAwesomeIcon icon={faGithub} size="2x" />
       </a>
       
 
-      <a href="https://www.youtube.com/c/jamesqquick"
+      <a href="https://www.youtube.com/"
   className="youtube social">
   <FontAwesomeIcon icon={faYoutube} size="2x" />
 </a>
-<a href="https://www.facebook.com/learnbuildteach/"
+<a href="https://www.facebook.com/"
   className="facebook social">
   <FontAwesomeIcon icon={faFacebook} size="2x" />
 </a>
-<a href="https://www.twitter.com/jamesqquick" className="twitter social">
+<a href="https://www.twitter.com/" className="twitter social">
   <FontAwesomeIcon icon={faTwitter} size="2x" />
     </a>
-    <a href="https://www.instagram.com/learnbuildteach"
+    <a href="https://www.instagram.com"
   className="instagram social">
   <FontAwesomeIcon icon={faInstagram} size="2x" />
     </a> 
