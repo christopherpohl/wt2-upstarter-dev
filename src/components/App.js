0e13f9ab87a9fc734913319df819112178f3b1cd
EmreIpcioglu <73101924+EmreIpcioglu@users.noreply.github.com>
2021-07-05 23:03:37 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,33 @@
+
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col} from 'react-bootstrap';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
-//import BurgerMenu from './BurgerMenu';
-import Footer from './footer.js';
-import LoginScreen from './LoginScreen.js';
-
-import Login from './Login.js';
-
 import Swipe from './Swipe.js';
+import Footer from './Footer.js';
 import Burger from './Burger.js';
 import Profil from './Profil.js';
-import Buttons from  './Buttons.js'
+import Buttons from  './Buttons.js';
 
 
+import Login from "./Login";
+
+
+import LoginScreen from './LoginScreen.js';
+
+//import Settings from './Settings.js';
+import Dashboard from './Dashboard.js';
+
+import useFetch from "react-fetch-hook"
 
 
 function App() {
+ 
+  
   return (
+
     <div className="App">
       <link
         rel="stylesheet"
@@ -27,21 +35,11 @@ function App() {
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
         crossOrigin="anonymous"
       />
+
+      
+
       <Container fluid>  
       <Router>
-
-      <Row className="header">
-        <Col lg ="11"></Col> 
-          
-        </Row>
-        <Row className="middle">
-          <Col lg="3"></Col>
-          <Col lg="6">
-            <Route path="/swipe" component={Swipe} />
-            <Route path="/login" component={LoginScreen} />
-          </Col>
-        </Row>  
-
 
       <Row className="header">
       <Col>
@@ -58,6 +56,7 @@ function App() {
           <Route path="/profil" component={Profil} />
           <Route path="/landing" component={Buttons}/>          
           <Route path="/" component={Buttons} />
+
           </Switch>
         </Col>          
       </Row>
@@ -70,127 +69,8 @@ function App() {
       </Router>
       </Container>
     </div>
-  );
-  }
 
+  );
+}
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
