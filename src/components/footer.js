e5263541b8021332639acfbdc43004cc414da55e
EmreIpcioglu <73101924+EmreIpcioglu@users.noreply.github.com>
2021-07-05 23:00:15 +0200

    try

--- src/components/footer.js
+++ src/components/footer.js
@@ -54,33 +54,8 @@ function Footer() {
 
 
 
-    </footer >);
-    <div class="social-container">
-      
-      <a href="https://github.com/christopherpohl/wt2-upstarter-dev"
-        className="git social">
-        <FontAwesomeIcon icon={faGithub} size="1x" />
-      </a>
-      
-
-      <a href="https://www.youtube.com/"
-  className="youtube social">
-  <FontAwesomeIcon icon={faYoutube} size="1x" />
-</a>
-<a href="https://www.facebook.com/"
-  className="facebook social">
-  <FontAwesomeIcon icon={faFacebook} size="1x" />
-</a>
-<a href="https://www.twitter.com/" className="twitter social">
-  <FontAwesomeIcon icon={faTwitter} size="1x" />
-    </a>
-    <a href="https://www.instagram.com"
-  className="instagram social">
-  <FontAwesomeIcon icon={faInstagram} size="1x" />
-    </a> 
-    </div>
-    
-  );
+    </footer >
+    );
 }
 
 
